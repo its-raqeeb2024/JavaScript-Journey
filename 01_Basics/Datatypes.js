f0059ab5bf2b3e73b44b41ebe67ef67a70d5c497
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-24 10:18:44 +0000

    Commit-3 learnt basic datatypes

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,23 @@
+let name = "raqeeb" 
+let age = 18
+let temp = 23.5467
+let Islogged = true
+let Unknown = undefined
+let zero = null
+
+// let name = "raqeeb" inee string
+// let age = 18 inee number 
+// let temp = 23.5 inee float
+// let Islogged = true inee boolean
+// let Unknown = undefined inee undefined 
+// let zero = null // iska data type aako object
+
+// aur typeof()function se data ka type dekh sakte hein
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(temp));
+console.log(typeof(Islogged));
+console.log(typeof(Unknown));
+console.log(typeof(zero));
+

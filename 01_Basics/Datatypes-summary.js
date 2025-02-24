94ffc192f7ce3d8e0e59de6080370af03d882685
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-24 17:36:51 +0000

    data-type-summary

---
+++ 01_Basics/Datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive 
+
+// number 
+// boolean 
+// String
+// null 
+// undefined
+
+
+// non primitive
+
+// inki actual memory se value access karti
+
+// arrays
+// objects
+// functions 
+
+// const Id = Symbol("rqb")
+// const AnotherId = Symbol("rqb")
+// console.log(Id === AnotherId);
+
+// non primitives
+
+//arrays
+let Cars = ["honda","creta","kwid","duster"];
+console.log(Cars);
+console.log(Cars[1]);
+
+//objects
+let MyObj = {
+    Name : "raqeeb",
+    age : 23,
+    Isadult : true,
+}
+console.log(MyObj);
+
+//functions
+let Myfunc = function(){
+    console.log("hello");
+    
+}
+Myfunc()

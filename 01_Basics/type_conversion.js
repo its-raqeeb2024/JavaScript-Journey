15319eafd123998ddbe49b9dda24fc08441dc902
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-24 17:20:39 +0000

    commit-4 type_conversions

---
+++ 01_Basics/type_conversion.js
@@ -0,0 +1,16 @@
+let name = "raqeeb"
+// console.log(name);
+let numName = Number(name);
+// console.log(typeof(numName));
+// console.log(numName); // inee not a number(NaN) return karaara but datatype aako number
+// console.log(typeof(name));
+
+// type conversions kaise kare
+let number = 12
+// always use a variable which can tell its own meaning = captial first letter for the data type which ure converting into eg: Number , String,Boolen,Float,Null,Undefined.
+let Stringnumber  = String(number)
+// console.log(Stringnumber);
+// console.log(typeof(Stringnumber));
+
+let height = 6.1
+console.log(typeof(height));

0ddbe1d9d7a09ab151af2fce9bb70fd4b7d97cf5
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-25 14:37:55 +0000

    Strings -done

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,24 @@
+// console.log("hello" + " world"); outdated method for concatenation
+let name = "raqeeb"
+let age = 23
+// console.log(`hello my name is ${name} and my age is ${age}`)//newer version of doing string concatenation similar to fstrings in python.
+const MyString = new String("raqeeb")
+console.log(name);
+console.log(typeof(name));
+// console.log(MyString);
+console.log(typeof(MyString))
+console.log(MyString.length);
+console.log(MyString.toUpperCase())
+const newPassword = "    raqeeb     "
+console.log(newPassword);
+console.log(newPassword.trim())
+const url = "https://raqeeb.com/raqeeb%20shaik"
+console.log(url.replace('%20','-'))
+console.log(url.includes('raq'))
+const line = "hello everyone hope you are fine"
+const Line = "hello hello hello all"
+console.log(Line.search("all"))
+// console.log(Line.replaceAll("hello","#"))
+// console.log(line.split(' '))
+
+

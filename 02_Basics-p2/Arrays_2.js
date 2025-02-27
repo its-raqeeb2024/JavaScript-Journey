067502787d8b98b9695825a147c14c42c6a823cf
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-27 02:50:32 +0000

    Arrays-2-done

---
+++ 02_Basics-p2/Arrays_2.js
@@ -0,0 +1,25 @@
+// const M_heroes = ["thor","ironman","spidy"]
+// const D_heroes = ["superman","latern","flash"]
+
+// // M_heroes.push(D_heroes)
+// // console.log(M_heroes)
+
+// const All_heroes = M_heroes.concat(D_heroes)
+// console.log(All_heroes);
+// const a1 = ['a','b','c']
+// const a2 = ['d','e','f']
+// const a3 = ['g','h','i']
+// const combined_arr = [...a1,...a2,...a3] 
+// console.log(combined_arr);
+
+// const depth_arr = [1,2,3,[4,5],[1,2,[3,4]]]
+// const flat_arr = depth_arr.flat(Infinity)
+// console.log(flat_arr);
+// const arr = Array.from({name : "shyam"})
+// console.log(arr)
+
+// const score1 = 100
+// const score2 = 200
+// const score3 = 300
+// const arr4 = Array.of(score1,score2,score3)
+// console.log(arr4)

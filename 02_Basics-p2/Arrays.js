174513c6f3c649e1acfb838349cdba4c19095347
its-raqeeb2024 <114853196+its-raqeeb2024@users.noreply.github.com>
2025-02-27 02:01:42 +0000

    Arrays-done

---
+++ 02_Basics-p2/Arrays.js
@@ -0,0 +1,31 @@
+// const Myarr = [0,1,2,3,4,5]
+// const myHeroes = ["dad","hero1","hero2"]
+// console.log(myHeroes[0]);
+// const arr2 = new Array(1,2,3,4)
+// const last = arr2.length
+// // console.log(arr2[last-1])
+// arr2.push(5)
+// console.log(arr2);
+// arr2.pop(5)
+// console.log(arr2);
+// arr2.unshift(6)
+// console.log(arr2);
+// arr2.shift()
+// console.log(arr2);
+// console.log(arr2.includes(8))
+// console.log(arr2.indexOf(8))
+// const ArrtoString = arr2.join()
+// const StringtoArray = ArrtoString.split()
+// console.log(StringtoArray)
+// console.log(ArrtoString)
+
+// const Arr1 = [0,1,2,3,4,5]
+// console.log("A ",Arr1 );
+
+// const SliceArr = Arr1.slice(1,3)
+// console.log("B ",SliceArr);
+
+// const SpliceArr = Arr1.splice(1,3)
+// console.log("A ",Arr1 );
+// console.log("C ",SpliceArr);
+
